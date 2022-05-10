3d80b451354a1dacf32cb580aef9ddf72dd139c4
gsemir0418 <845217811@qq.com>
2022-05-10 14:22:51 +0800

    feat: 根据api概要设计创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get '/users/:id', to: 'users#show'
-  post '/users/', to: 'users#create'
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items 
+      resources :tags
+    end
+  end
 end
