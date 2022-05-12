90b573e7e649999ad6a96ece8c9f3f78a3dd92d6
gsemir0418 <845217811@qq.com>
2022-05-12 16:19:03 +0800

    BDD for validation_code controller

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    head 201
+    validation_code = ValidationCode.new email: params[:email],
+      kind: 'log_in', code: SecureRandom.random_number.to_s[2..7]
+    if validation_code.save
+      head 200
+    else 
+      render json: {errors: validation_code.errors}
+    end
+    # TODO:目前仅实现在数据库中创建了validation_code，真正的发送功能待实现
   end
 end
---
+++ spec/requests/validation_codes_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "ValidationCodes", type: :request do
+  describe "验证码" do
+    it "能够被发送" do
+      post '/api/v1/validation_codes', params: {
+        email: '845217811@qq.com'
+      }
+      expect(response).to have_http_status(200)
+    end
+  end
+end
