224ba7409e0062d2dc823927fd962b389c12d66f
gsemir0418 <845217811@qq.com>
2022-05-17 21:30:30 +0800

    完善部署命令

--- bin/deploy_for_host.sh
+++ bin/deploy_for_host.sh
@@ -1,13 +1,13 @@
 dir=oh-my-env
-# 打包时间作为当前版本号
+# 以时间作为版本 注意空格
 time=$(date +"%Y%m%d-%H%M%S")
-# 打包的目标路径
 dist=tmp/mangosteen-$time.tar.gz
 current_dir=$(dirname $0)
+# 打包的目标路径
 deploy_dir=/workspaces/$dir/mangosteen_deploy
 
 # 删除之前存在的
-yes | rm tem/mangosteen-*.tar.gz;
+yes | rm tmp/mangosteen-*.tar.gz;
 yes | rm $deploy_dir/mangosteen-*.tar.gz;
 
 # 打包 除了tmp/cache中的文件 打包到dist目录 *表示所有不以点开头的文件
@@ -16,7 +16,7 @@ mkdir -p $deploy_dir
 # 将Dockerfile与docker命令文件复制过去
 cp $current_dir/../config/host.Dockerfile $deploy_dir/Dockerfile
 cp $current_dir/setup_host.sh $deploy_dir/
+# 将dist文件（tar包移动出来）
 mv $dist $deploy_dir
 echo $time > $deploy_dir/version
-echo $time
 echo 'DONE'
--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -4,9 +4,13 @@ version=$(cat mangosteen_deploy/version)
 
 echo 'docker build ...'
 docker build mangosteen_deploy -t mangosteen:$version
+if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
+  echo 'docker rm ...'
+  docker rm -f $container_name
+fi
 echo 'docker run ...'
 docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
-echo 'docker exec ...'
+# echo 'docker exec ...'
 # 创建数据库 同步数据表
-docker exec -it $container_name bin/rails db:create db:migrate
+# docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE'
