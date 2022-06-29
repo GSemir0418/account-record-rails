a8841bf3baa06dbc0c8b7ca9cb826a841b939c2d
gsemir0418 <845217811@qq.com>
2022-06-29 15:40:09 +0800

    利用生命周期函数优化Controller代码

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,3 +1,16 @@
 class ValidationCode < ApplicationRecord
     validates :email, presence: true
+
+    # 在创建这条记录之前，调用generate_code方法生成随机code
+    before_create :generate_code
+    # 在save这条记录之后，调用send_email方法发送邮件
+    after_create :send_email
+
+    def generate_code
+        # self相当于js中的this，指代当前实例对象
+        self.code = SecureRandom.random_number.to_s[2..7]
+    end
+    def send_email
+        UserMailer.welcome_email(self.email)
+    end
 end
