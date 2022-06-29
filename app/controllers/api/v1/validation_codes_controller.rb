d73cca5c11829bcad6fce2b779a30fdaf59fece4
gsemir0418 <845217811@qq.com>
2022-06-29 20:50:47 +0800

    TDD防止请求重复发送

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
+    # 重复请求验证码的校验
+    if ValidationCode.exists?(email: params[:email], kind: 'sign_in', created_at: 1.minute.ago..Time.now)
+      render status: 429
+      return 
+    end
     validation_code = ValidationCode.new email: params[:email], kind: 'sign_in'
       if validation_code.save
         render status: 200
--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "ValidationCodes", type: :request do
   describe "验证码" do
-    it "能够被发送" do
+    it "60秒内不能重复发送" do
       post '/api/v1/validation_codes', params: {
         email: '845217811@qq.com'
       }
       expect(response).to have_http_status(200)
+      post '/api/v1/validation_codes', params: {
+        email: '845217811@qq.com'
+      }
+      expect(response).to have_http_status(429)
     end
   end
 end
