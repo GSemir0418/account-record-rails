87ad9879c13abc61e8fdfcdbe4d4ad68c6529f7c
gsemir0418 <845217811@qq.com>
2022-06-29 20:22:53 +0800

    优化validation_code排序查询code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
     def welcome_email(email)
-        # 根据传入的email到数据库中查询响应的code
-        validation_code = ValidationCode.find_by_email(email)
+        # 先按创建时间排序，以确保code是最新的
+        # 再根据传入的email到数据库中查询响应的code
+        validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
         # 给模板传递变量
         @code = validation_code.code
         # 发送邮件
