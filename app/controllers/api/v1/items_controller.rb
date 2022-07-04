b69e402d8e1b6e3c0979170bb0c6462dacfff0b6
gsemir0418 <845217811@qq.com>
2022-07-04 16:31:11 +0800

    初步完成api/vi/items筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
     def index
-        items = Item.page params[:page]
+        # ruby使用A..B表示范围
+        items = Item.where({created_at: params[:created_after]..params[:created_before]}).page params[:page]
         # 也可以Item.page(params[:page]).per(100)自定义pageSize
         render json: { resources: items, pager: {
           page: params[:page],
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -24,6 +24,19 @@ RSpec.describe "Items", type: :request do
       json = JSON.parse response.body
       expect(json['resources'].size).to eq 1
     end
+    it "按时间筛选" do
+      # 三条数据 两条符合
+      item1 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
+      item2 = Item.create amount: 100, created_at: Time.new(2018, 1, 2)
+      item3 = Item.create amount: 100, created_at: Time.new(2019, 1, 2)
+      # 按时间筛选
+      get '/api/v1/items?created_after=2018-01-01&created_before=2018-01-03'
+      expect(response).to have_http_status(200)
+      json = JSON.parse response.body
+      expect(json['resources'].size).to eq 2
+      expect(json['resources'][0]['id']).to eq item1.id
+      expect(json['resources'][1]['id']).to eq item2.id
+    end
   end
 
   describe 'POST /items' do
