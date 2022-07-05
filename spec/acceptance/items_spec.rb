38a829793c36714f4c44985dbe34b61adca76d87
gsemir0418 <845217811@qq.com>
2022-07-05 12:07:39 +0800

    更新文档，添加鉴权相关内容

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -3,25 +3,21 @@ require 'rspec_api_documentation/dsl'
 
 resource "账目" do
     get "/api/v1/items" do
-        # 描述请求 parameter
+        # 指定验证方式 基础验证（Bear） 值用变量表示
+        authentication :basic, :auth
         parameter :page, '页码'
         parameter :created_after, '创建时间起点（筛选条件）'
         parameter :created_before, '创建时间终点（筛选条件）'
-
-        # 描述响应 response_field
-        # response_field :id, 'ID', scope: :resources
-        # response_field :amount, '金额（单位：分）', scope: :resources
-        # 以上两句有相同的scope，所以可以简写为下面的
         with_options :scope => :resources do
             response_field :id, 'ID'
             response_field :amount, '金额（单位：分）'
         end
-
-        # 构造示例请求
         let(:created_after) {'2021-01-01'}
         let(:created_before) {'2022-01-01'}
+        let(:current_user) { User.create email: '1@qq.com' }
+        let(:auth) { "Bearer #{current_user.generate_jwt}" }
         example "获取账目" do
-            11.times do Item.create amount: 100, created_at: '2021-6-30' end
+            11.times do Item.create amount: 100, created_at: '2021-06-30', user_id: current_user.id end
             do_request
             expect(status).to eq 200
             json = JSON.parse response_body
