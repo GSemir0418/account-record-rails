7e7871152246f865a644d0d208b3fde2d0598f56
gsemir0418 <845217811@qq.com>
2022-07-07 11:05:29 +0800

    TDD完成创建标签Api

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,12 +1,24 @@
 class Api::V1::TagsController < ApplicationController
     def index
         current_user = User.find request.env['current_user_id']
-        return render status: :not_found if current_user.nil?
+        return render status: 401 if current_user.nil?
         tags = Tag.where({user_id: current_user.id}).page(params[:page])
         render json: {resources: tags, pager: {
             page: params[:page] || 1,
             per_page: Tag.default_per_page,
             count: Tag.count
-        }}
+            }}
+    end
+    def create
+        current_user = User.find request.env['current_user_id']
+        return render status: 401 if current_user.nil?
+        # new表示仅创建tag实例，create表示在new + save
+        tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user.id
+        # save会返回true/false 而create则会直接报错
+        if tag.save
+            render json:{resource: tag}
+        else
+            render json:{errors: tag.errors}, status: :unprocessable_entity
+        end
     end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,6 @@
 class Tag < ApplicationRecord
+  # 在model层添加限制 让ruby报错而不是数据库报错
+  validates :name, presence: true
+  validates :sign, presence: true
   belongs_to :user
 end
