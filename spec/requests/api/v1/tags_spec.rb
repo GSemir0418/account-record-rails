47d94dc3117abbaab4bb126c118784524f3290bb
gsemir0418 <845217811@qq.com>
2022-07-07 10:34:41 +0800

    TDD实现获取tag接口

---
+++ spec/requests/api/v1/tags_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Tags", type: :request do
+  describe "获取标签" do
+    it "未登录获取标签" do
+      get '/api/v1/tags'
+      expect(response).to have_http_status 401
+    end
+    it "登录后分页获取标签" do
+      user = User.create email: '1@qq.com'
+      another_user = User.create email: '2@qq.com'
+      11.times do |i| Tag.create name: "tag#{i}", sign: 'x', user_id: user.id end
+      11.times do |i| Tag.create name: "tag#{i}", sign: 'x', user_id: another_user.id end
+      get '/api/v1/tags', headers: user.generate_auth_header
+      expect(response).to have_http_status 200
+      json = JSON.parse response.body
+      expect(json['resources'].size).to eq 10
+      get '/api/v1/tags', headers: user.generate_auth_header, params: {page: 2}
+      expect(response).to have_http_status 200
+      json = JSON.parse response.body
+      expect(json['resources'].size).to eq 1
+    end
+  end
+end
