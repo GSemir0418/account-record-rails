895ed3d40ecae706a08cf9a0211c84b702e4e3b1
gsemir0418 <845217811@qq.com>
2022-07-13 14:37:32 +0800

    配置生产环境smtp配置，解决验证码发送的bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     def welcome_email(email)
         # 先按创建时间排序，以确保code是最新的
         # 再根据传入的email到数据库中查询响应的code
-        validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
+        validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
         # 给模板传递变量
         @code = validation_code.code
         # 发送邮件
